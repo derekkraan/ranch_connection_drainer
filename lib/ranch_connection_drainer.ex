a52796a16903d77ba310e8178f70a40324417fb7
Derek Kraan <derek.kraan@gmail.com>
2019-01-22 16:36:39 +0100

    Improve docs

--- lib/ranch_connection_drainer.ex
+++ lib/ranch_connection_drainer.ex
@@ -5,10 +5,21 @@ defmodule RanchConnectionDrainer do
   If your Phoenix endpoint is `MyPhoenix.Endpoint`, and you are running your server on http, then the correct value is `MyPhoenix.Endpoint.HTTP`. If you are running https, then you would use `MyPhoenix.Endpoint.HTTPS`.
 
   To add this to your application, simply add `{RanchConnectionDrainer, [ranch_ref: MyPhoenix.Endpoint.HTTP, shutdown: 10_000]}` to the line *below* `MyPhoenix.Endpoint` in your Application file.
+
+  Example:
+
+  ```
+  children = [
+    MyPhoenixProject.Endpoint,
+    {RanchConnectionDrainer, ranch_ref: MyPhoenixProject.Endpoint.HTTP, shutdown: 30_000}
+  ]
+  Supervisor.init(children, opts)
+  ```
   """
 
   use GenServer
 
+  @spec child_spec(options :: keyword()) :: Supervisor.child_spec()
   def child_spec(options) when is_list(options) do
     %{
       id: __MODULE__,
@@ -17,10 +28,12 @@ defmodule RanchConnectionDrainer do
     }
   end
 
+  @doc false
   def start_link(ranch_ref) do
     GenServer.start_link(__MODULE__, ranch_ref)
   end
 
+  @doc false
   def init(ranch_ref) do
     Process.flag(:trap_exit, true)
     {:ok, ranch_ref}
