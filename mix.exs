84a44309ea5a0239f98db0d99f0803170cd67b90
Derek Kraan <derek.kraan@gmail.com>
2019-01-23 13:25:05 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RanchConnectionDrainer.MixProject do
   def project do
     [
       app: :ranch_connection_drainer,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
